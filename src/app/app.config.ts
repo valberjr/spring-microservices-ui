4a629ff9a1fc0d2a1ef501b4cbf00ef120d413c3
valberjr <valber@duck.com>
2024-08-08 14:34:12 -0300

    feat: enable HTTP Client, OAuth2, and interceptor functionalities

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAuth } from 'angular-auth-oidc-client';
 import { routes } from './app.routes';
+import { authConfig } from './config/auth-config';
+import { authInterceptor } from './interceptor/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideAuth(authConfig),
+  ],
 };
