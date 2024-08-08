ba852f469128b495472d88c16bf6c328970eede0
valberjr <valber@duck.com>
2024-08-08 13:55:26 -0300

    feat: add HTTP interceptor to include authentication token in all outgoing requests

---
+++ src/app/interceptor/auth.interceptor.ts
@@ -0,0 +1,19 @@
+import { HttpInterceptor, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(OidcSecurityService);
+
+  authService.getAccessToken().subscribe((token) => {
+    if (token) {
+      let header = 'Bearer ' + token;
+      let headers = req.headers.set('Authorization', header);
+      req = req.clone({ headers });
+      return next(req);
+    }
+    return next(req);
+  });
+
+  return next(req);
+};
